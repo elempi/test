0c566a881daa654e9ef7c956a4bfb7a7b4e9a8a9
elempi <dip@elempi.com>
2018-03-26 11:45:38 +0300

    dsfdsfds sdafdsf

--- test.js
+++ test.js
@@ -1,1 +1,300 @@
 dfsgfd
+C:\Users\KM\git>git commit -m "first commit"
+[master 5200c97] first commit
+ 1 file changed, 1 insertion(+)
+ create mode 100644 README.md
+
+C:\Users\KM\git>git remote add origin https://github.com/elempi/test.git
+
+C:\Users\KM\git>git push -u origin master
+fatal: unable to access 'https://github.com/elempi/test.git/': SSL certificate p
+roblem: unable to get local issuer certificate
+
+C:\Users\KM\git>Microsoft Windows [Version 6.1.7601]
+'Microsoft' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
+'Copyright' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM>git
+'C:\Users\KM' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>usage: git [--version] [--help] [-C <path>] [-c name=value]
+The system cannot find the file specified.
+
+C:\Users\KM\git>           [--exec-path[=<path>]] [--html-path] [--man-path] [--
+info-path]
+The system cannot find the file specified.
+
+C:\Users\KM\git>           [-p | --paginate | --no-pager] [--no-replace-objects]
+ [--bare]
+'[-p' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>           [--git-dir=<path>] [--work-tree=<path>] [--namespace=
+<name>]
+The system cannot find the file specified.
+
+C:\Users\KM\git>           <command> [<args>]
+The syntax of the command is incorrect.
+
+C:\Users\KM\git>
+C:\Users\KM\git>These are common Git commands used in various situations:
+'These' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>start a working area (see also: git help tutorial)
+The system cannot find the file a.
+
+C:\Users\KM\git>   clone      Clone a repository into a new directory
+'clone' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   init       Create an empty Git repository or reinitialize an
+existing one
+'init' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>work on the current change (see also: git help everyday)
+'work' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   add        Add file contents to the index
+'add' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   mv         Move or rename a file, a directory, or a symlink
+'mv' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   reset      Reset current HEAD to the specified state
+Invalid parameter(s)
+RESET { SESSION }
+
+C:\Users\KM\git>   rm         Remove files from the working tree and from the in
+dex
+'rm' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>examine the history and state (see also: git help revisions)
+'examine' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   bisect     Use binary search to find the commit that introduc
+ed a bug
+'bisect' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   grep       Print lines matching a pattern
+'grep' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   log        Show commit logs
+'log' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   show       Show various types of objects
+'show' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   status     Show the working tree status
+'status' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>grow, mark and tweak your common history
+'grow' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   branch     List, create, or delete branches
+'branch' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   checkout   Switch branches or restore working tree files
+'checkout' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   commit     Record changes to the repository
+'commit' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   diff       Show changes between commits, commit and working t
+ree, etc
+'diff' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   merge      Join two or more development histories together
+'merge' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   rebase     Reapply commits on top of another base tip
+'rebase' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   tag        Create, list, delete or verify a tag object signed
+ with GPG
+'tag' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>collaborate (see also: git help workflows)
+'collaborate' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   fetch      Download objects and refs from another repository
+'fetch' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>   pull       Fetch from and integrate with another repository o
+r a local branch
+'pull' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>   push       Update remote refs along with associated objects
+'push' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>'git help -a' and 'git help -g' list available subcommands and s
+ome
+''git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>concept guides. See 'git help <command>' or 'git help <concept>'
+
+The system cannot find the file specified.
+
+C:\Users\KM\git>to read about a specific subcommand or concept.
+'to' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM>git config --global user.email "dip@elempi.com"
+'C:\Users\KM' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM>git config --global user.name "elempi"
+'C:\Users\KM' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM>git init
+'C:\Users\KM' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>Initialized empty Git repository in C:/Users/KM/.git/
+'Initialized' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM>cd git
+'C:\Users\KM' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git init
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>Reinitialized existing Git repository in C:/Users/KM/git/.git/
+'Reinitialized' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git add README.md
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>fatal: pathspec 'README.md' did not match any files
+'fatal:' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>echo "# test" >> README.md
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git add README.md
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git commin -m "first commit"
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>git: 'commin' is not a git command. See 'git --help'.
+'git:' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>Did you mean this?
+'Did' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>        commit
+'commit' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>y
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>'y' is not recognized as an internal or external command,
+''y'' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>operable program or batch file.
+'operable' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git commit -m "first commit"
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>[master 5200c97] first commit
+'[master' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git> 1 file changed, 1 insertion(+)
+'1' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git> create mode 100644 README.md
+'create' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git remote add origin https://github.com/elempi/
+test.git
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>git push -u origin master
+'C:\Users\KM\git' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>fatal: unable to access 'https://github.com/elempi/test.git/': S
+SL certificate p
+'fatal:' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>roblem: unable to get local issuer certificate
+'roblem:' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\KM\git>
+C:\Users\KM\git>C:\Users\KM\git>
